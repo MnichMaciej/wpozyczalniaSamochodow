b7e26ee7c0468e03061104781522f95934a7aa8a
mnichmaciej44 <mnichmaciej44@live.com>
2020-12-29 14:55:09 +0100

    Add TDD project

---
+++ wypozyczalniaSamochodowTesty/LoginPanelTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using wypozyczalniaSamochodow;
+using System.Windows.Forms;
+
+namespace wypozyczalniaSamochodowTesty
+{
+    [TestClass]
+    public class LoginPanelTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var app = new App();
+            var loginpanel = new LoginPanel();
+            loginpanel.setParent(app);
+            Assert.AreEqual(app, loginpanel.parent);
+        }
+    }
+}
